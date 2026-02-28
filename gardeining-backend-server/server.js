18f1319b00d0263af2e8fdc65c165a518b4698d5
sangeetadey99 <sdey6268@gmail.com>
2026-02-28 21:35:09 +0530

    added for render

--- gardeining-backend-server/server.js
+++ gardeining-backend-server/server.js
@@ -19,7 +19,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
